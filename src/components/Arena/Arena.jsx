7a262b130158ae7d73703268b93816a31b4bb739
wayungi <fwayungi@gmail.com>
2023-05-11 23:01:58 +0300

    ADD: phaser code template

--- src/components/Arena/Arena.jsx
+++ src/components/Arena/Arena.jsx
@@ -1,6 +1,39 @@
 import { ArenaWrapp } from './Arena.styled';
+import React, { useEffect } from 'react';
+import Phaser from 'phaser';
 
 const Arena = () => {
+  useEffect(() => {
+    const config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      scene: {
+        preload: preload,
+        create: create,
+        update: update,
+      },
+    };
+
+    const game = new Phaser.Game(config);
+
+    function preload() {
+      // Load game assets here
+    }
+
+    function create() {
+      // Create game objects here
+    }
+
+    function update() {
+      // Update game state here
+    }
+
+    return () => {
+      game.destroy();
+    };
+  }, []);
+
   return <ArenaWrapp>Arena</ArenaWrapp>;
 };
 
