2eef1a04d7db5fbfd86fbc3867c8c90031b3f143
wayungi <fwayungi@gmail.com>
2023-05-15 22:20:15 +0300

    ADD: bots images to scene

--- src/components/Scenes/SceneOne.js
+++ src/components/Scenes/SceneOne.js
@@ -1,8 +1,8 @@
 import Phaser from 'phaser';
 import Game_background from '../../images/game/bg_main.png';
-// import botOne from '../../images/ships/bot_1.png';
-// import botTwo from '../../images/ships/bot_2.png';
-import bunny from '../../images/ships/bunny.png';
+// import botOne from 'images/bot_1.png';
+// import botTwo from 'images/bot_2.png';
+// import bunny from '../../images/game/bunny.png';
 
 class SceneOne extends Phaser.Scene {
     constructor() {
@@ -12,17 +12,17 @@ class SceneOne extends Phaser.Scene {
     preload() {
       // load background image
       this.load.image('game_bg', Game_background);
-      this.load.image('rabbit', bunny);
-      // this.load.image('bot1', botOne);
-      // this.load.image('bot2', botTwo);
+      // this.load.image('rabbit', 'images/bunny.png');
+      this.load.image('bot1', 'images/bot_1.png');
+      this.load.image('bot2', 'images/bot_2.png');
     }
 
     create() {
       // Game logic
-      // this.add.image(10,10, 'bot1').setScale(0.2);
-      // this.add.image(50,70, 'bot2').setScale(0.2);
       this.add.image(0, 0, 'game_bg');
-      this.add.image(20,20, 'rabbit');
+      this.add.image(80,120, 'bot1').setScale(0.5)
+      this.add.image(50,70, 'bot2').setScale(0.5);
+      // this.add.image(80,80, 'rabbit');
 
 
 
