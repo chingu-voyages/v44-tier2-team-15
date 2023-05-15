f398e495b18e73536c55f19be50a67fa57425637
wayungi <fwayungi@gmail.com>
2023-05-15 22:22:14 +0300

    REMOVE: commented code & unused images

--- src/components/Scenes/SceneOne.js
+++ src/components/Scenes/SceneOne.js
@@ -1,8 +1,6 @@
 import Phaser from 'phaser';
 import Game_background from '../../images/game/bg_main.png';
-// import botOne from 'images/bot_1.png';
-// import botTwo from 'images/bot_2.png';
-// import bunny from '../../images/game/bunny.png';
+
 
 class SceneOne extends Phaser.Scene {
     constructor() {
@@ -12,7 +10,7 @@ class SceneOne extends Phaser.Scene {
     preload() {
       // load background image
       this.load.image('game_bg', Game_background);
-      // this.load.image('rabbit', 'images/bunny.png');
+      // the images below should be placed in the public directory
       this.load.image('bot1', 'images/bot_1.png');
       this.load.image('bot2', 'images/bot_2.png');
     }
@@ -22,7 +20,6 @@ class SceneOne extends Phaser.Scene {
       this.add.image(0, 0, 'game_bg');
       this.add.image(80,120, 'bot1').setScale(0.5)
       this.add.image(50,70, 'bot2').setScale(0.5);
-      // this.add.image(80,80, 'rabbit');
 
 
 
