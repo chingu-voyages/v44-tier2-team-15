254f701d58dab692be894b81227d4fd171ddfed6
wayungi <fwayungi@gmail.com>
2023-05-15 23:48:51 +0300

    ADD: collision between bots

--- src/components/Scenes/SceneOne.js
+++ src/components/Scenes/SceneOne.js
@@ -29,13 +29,17 @@ class SceneOne extends Phaser.Scene {
       this.ship2 = this.physics.add.image(50,70, 'bot2').setScale(0.5);
 
       // add motion to the images
-      this.ship1.setVelocity(100, 0); // Moves the image horizontally at a speed of 100 pixels per second
+      this.ship1.setVelocity(100, 20); // Moves the image horizontally at a speed of 100 pixels per second
       this.ship1.setBounce(1, 1);
       this.ship1.setCollideWorldBounds(true);
 
       this.ship2.setVelocity(100, 100); // Moves the image horizontally at a speed of 100 pixels per second
       this.ship2.setBounce(1, 1);
       this.ship2.setCollideWorldBounds(true);
+
+      // add collision between bots
+      this.physics.add.collider(this.ship1, this.ship2)
+
 
 
     }
