a3859fb48a6c930458f003c87b83ae608159fa25
olhazamlynska <olya.sonechko@gmail.com>
2023-05-25 12:34:20 +0300

    add cross icon

--- src/components/ConfigurationPanel/ConfigurationPanel.js
+++ src/components/ConfigurationPanel/ConfigurationPanel.js
@@ -1,9 +1,15 @@
 import ConfigurationForm from 'components/ConfigurationPanel/ConfigurationForm';
-import { Title } from './ConfigurationPanel.styled';
-
+import { Title, Icon } from './ConfigurationPanel.styled';
+import { IconContext } from 'react-icons';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 const ConfigurationPanel = ({ onClose }) => {
   return (
     <>
+      <IconContext.Provider value={{ size: '30px', color: '#2a3a42' }}>
+        <Icon onClick={onClose}>
+          <AiOutlineCloseCircle />
+        </Icon>
+      </IconContext.Provider>
       <Title>Choose configuration</Title>
       <ConfigurationForm onClose={onClose} />
     </>
